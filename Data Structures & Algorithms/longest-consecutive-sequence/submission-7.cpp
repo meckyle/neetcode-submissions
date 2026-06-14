b5f40060f6fe2f015b06975b54cc14efff9d31c5
meckyle <150486999+meckyle@users.noreply.github.com>
2026-06-14 17:45:58 -0600

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int>mp;
+
+        int counter = 0;
+        int highest = 1;
+
+
+
+        
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+           // if(nums[i] == nums[i+1] + 1){
+                //counter++;
+           // }
+
+        }
+
+        for (int i = 0; i < nums.size(); i++){
+
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+        }
+
+            if(mp.find(nums[i] + 1) != mp.end()){
+                highest++;
+
+            }
+            else{
+                if(highest >= counter){
+                    counter = highest;
+                }
+                highest = 1;
+            }
+        }
+
+
+        return counter;
+
+
+
+        //if (mp.find(5) != mp.end()) 
+
+        
+
+        
+    }
+};
